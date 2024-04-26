826c3306b7e33fff10dc38ae2ff05115b4ca2484
vhle12 <158373104+vhle12@users.noreply.github.com>
2024-04-26 15:12:03 -0400

    complete 1 & 2

--- phase-1/get/beginning/index.js
+++ phase-1/get/beginning/index.js
@@ -11,9 +11,29 @@ Objectives
 */
 
 // A synchronous request to https://dog-api.kinduff.com/api/facts?number=1.
+fetch("https://dog-api.kinduff.com/api/facts?number=1") // Make a GET request for dog facts
+.then((response) => response.json()) // Convert the JSON response into JS. // the success condition is to convert json to JS
+.then((data) => {
+    const span = document.querySelector("#dog");
+    span.textContent = data.facts[0];
+})
+.catch((error) => { console.log(error) }); 
 
-// A synchronous request to https://anapioficeandfire.com/api/books.
+// A synchronous request to .
+fetch("https://anapioficeandfire.com/api/books") // make request for pokemon
+.then(response => response.json()) // converts JSON response to JS
+.then(books => {
+    const got = document.querySelector("#got");
+    books.forEach((book) => {
+        const listItem = document.createElement("li");
+        listItem.textContent = book.name;
+        got.append(listItem);
+    });
+})
+.catch((error) => {console.log(error); });
 
 // An asynchronous request to https://pokeapi.co/api/v2/pokemon/[name]"
 
 // ~ Challenge: Make a GET request to an API of your choice!
+
+fetch()
